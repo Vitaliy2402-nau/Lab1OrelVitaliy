67ad659562a122f9800c126079782926c51d19b5
Vitaliy2402-nau <78072344+Vitaliy2402-nau@users.noreply.github.com>
2021-06-14 12:25:46 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	float a,b,c;
+	cin>>a;
+	cin>>b;
+	c=a+b;
+	cout<<"a+b="<<c;
+	c=a-b;
+	cout<<"a-b="<<c;
+	c=a*b;
+	cout<<"a*b="<<c;
+	c=a/b;
+	cout<<"a/b="<<c;
+	return 0;
+}
